43c33206d8407b21939bbe326c695697081f5e6e
Bhupendra <bhupendra.j30@gmail.com>
2015-08-17 22:24:46 +0530

    js delete

--- mine.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-      var pars = $("p");
-      for( i=0; i<pars.length; i++ ){
-          alert("Found paragraph: " + pars[i].innerHTML);
-      }
-      alert($("#myid").html());
-   });
